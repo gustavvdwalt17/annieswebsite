de4289e865affaa3327ae2c37f9df3080890f360
Gustav Van Der Walt <goosyvdwalt@gmail.com>
2023-12-31 19:30:39 +0200

    final

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/bosimg" element={<SuikerBekkieBosImageGallery />} />
         <Route
